6eb4ec6cde029a5312aa15343520a1d0884f6870
vitaliy-1111 <networkvetalius@gmail.com>
2022-01-29 19:31:10 +0000

    Deploying to gh-pages from @ vitaliy-1111/team-project-filmoteka@d0a42ac62220eee69fe7575a5ce5ba6f99a9e410 🚀

--- src.088d554c.js
+++ src.088d554c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-filmoteka/src.bf7c4970.js.map
+//# sourceMappingURL=/team-project-filmoteka/src.088d554c.js.map

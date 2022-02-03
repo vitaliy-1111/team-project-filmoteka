7b53d4f32001708012af0e5c32a03c550eef6b1e
Denis Fedash <0501054104d@gmail.com>
2022-02-03 21:44:46 +0200

    add genres

--- src/js/gallery/gallery-tpl.js
+++ src/js/gallery/gallery-tpl.js
@@ -1,21 +1,27 @@
+import { allGenres, renderGenres } from './genres'
+
 const refs = {
-    cardList: document.querySelector('.card-list')
+  cardList: document.querySelector('.card-list'),
+  contentText: document.querySelector('.content')
 }
 
 export function MoviesCards (results){
   const markup = results.map(
     ({ title, id, poster_path, release_date, genre_ids }) => {
-      return `<li class="list card-item">
+      const genreName = renderGenres(genre_ids, allGenres)
+        return `<li class="list card-item">
             <a href="" class="link"></a>
             <div class="image_thumb">
                 <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${title}" id="${id}" loading="lazy">
                 <div class="content">
                 <h2 class="content_title">${title}</h2>
-                <p class="content_text">${genre_ids} | ${release_date}</p>
+                <p class="content_text">${genreName} | ${release_date}</p>
             </div>
             </div>
             
         </li>`}).join(' ');
   refs.cardList.innerHTML = markup;
 };
+
+
 

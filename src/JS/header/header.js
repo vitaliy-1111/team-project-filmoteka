047266980fc9c692a14b122d6647a25603094bd4
Igor Shapoval <azzgard1992@gmail.com>
2022-02-04 10:52:25 +0200

    header-fixed

--- src/JS/header/header.js
+++ src/JS/header/header.js
@@ -2,37 +2,41 @@ const refs = {
   home: document.querySelector('.menu__list-link--home'),
   library: document.querySelector('.menu__list-link--library'),
   header: document.querySelector('.page-header'),
+  logo: document.querySelector('.menu__logo'),
 };
 
 refs.library.addEventListener('click', onLibrary);
 refs.home.addEventListener('click', onHome);
+refs.logo.addEventListener('click', onHome);
 
 function onLibrary(e) {
   e.preventDefault();
   refs.header.innerHTML = markupLibrary();
-  console.log('onLibrary');
+
   document.querySelector('.menu__list-link--library').addEventListener('click', onLibrary);
   document.querySelector('.menu__list-link--home').addEventListener('click', onHome);
+  document.querySelector('.menu__logo').addEventListener('click', onHome);
 }
 
 function onHome(e) {
   e.preventDefault();
   refs.header.innerHTML = markupHome();
-  console.log('onHome');
   document.querySelector('.menu__list-link--home').addEventListener('click', onHome);
   document.querySelector('.menu__list-link--library').addEventListener('click', onLibrary);
+  document.querySelector('.menu__logo').addEventListener('click', onHome);
 }
 
 function markupLibrary() {
   return `<div class="container library-page-header__container">
     <div class="menu">
-      <a href="" class="logo link">
-        <svg width="24" height="24">
-          <use href="./images/icons.svg#logo"></use>
-        </svg>
-        <span class="logo_label">Filmoteka</span>
+      <a href="" class="menu__logo link">
+        <img
+          alt="logo"
+          src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTE5LjgyIDJINC4xOEEyLjE4IDIuMTggMCAwIDAgMiA0LjE4djE1LjY0QTIuMTggMi4xOCAwIDAgMCA0LjE4IDIyaDE1LjY0QTIuMTggMi4xOCAwIDAgMCAyMiAxOS44MlY0LjE4QTIuMTggMi4xOCAwIDAgMCAxOS44MiAyWk03IDJ2MjBNMTcgMnYyME0yIDEyaDIwTTIgN2g1TTIgMTdoNU0xNyAxN2g1TTE3IDdoNSIgc3Ryb2tlPSIjODE4MTgxIiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPjwvc3ZnPg=="
+        />
+        <span class="menu__logo-label">Filmoteka</span>
       </a>
-      <ul class="menu__list">
+      <ul class="menu__list list">
         <li class="menu__list-item">
           <a href="" class="menu__list-link menu__list-link--home  link"
             >Home</a>
@@ -52,11 +56,12 @@ function markupLibrary() {
 function markupHome() {
   return `<div class="container page-header__container">
     <div class="menu">
-      <a href="/" class="logo link">
-        <svg width="24" height="24">
-          <use href="./images/icons.svg#logo"></use>
-        </svg>
-        <span class="logo_label">Filmoteka</span>
+      <a href="/" class="menu__logo link">
+        <img
+          alt="logo"
+          src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTE5LjgyIDJINC4xOEEyLjE4IDIuMTggMCAwIDAgMiA0LjE4djE1LjY0QTIuMTggMi4xOCAwIDAgMCA0LjE4IDIyaDE1LjY0QTIuMTggMi4xOCAwIDAgMCAyMiAxOS44MlY0LjE4QTIuMTggMi4xOCAwIDAgMCAxOS44MiAyWk03IDJ2MjBNMTcgMnYyME0yIDEyaDIwTTIgN2g1TTIgMTdoNU0xNyAxN2g1TTE3IDdoNSIgc3Ryb2tlPSIjODE4MTgxIiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPjwvc3ZnPg=="
+        />
+        <span class="menu__logo-label">Filmoteka</span>
       </a>
       <ul class="menu__list list">
         <li class="menu__list-item">
@@ -70,9 +75,11 @@ function markupHome() {
     </div>
     <div class="form-box">
       <form class="form">
-        <svg class="form__icon" width="12" height="12">
-          <use href="./images/icons.svg#search"></use>
-        </svg>
+        <img
+          class="form__icon"
+          alt="search"
+          src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTIiIGhlaWdodD0iMTIiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTUuNSA5LjVhNCA0IDAgMSAwIDAtOCA0IDQgMCAwIDAgMCA4Wk0xMC41IDEwLjUgOC4zMjUgOC4zMjUiIHN0cm9rZT0iI2ZmZiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+PC9zdmc+"
+        />
         <input type="text" name="text" class="search" placeholder="Поиск фильмов" />
       </form>
       <span class="form__error-message"></span>

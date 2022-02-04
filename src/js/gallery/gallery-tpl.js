0182a10b464b7b98ac681e62d65e57a27928328a
Denis Fedash <0501054104d@gmail.com>
2022-02-04 20:25:31 +0200

    fix bugs

--- src/js/gallery/gallery-tpl.js
+++ src/js/gallery/gallery-tpl.js
@@ -9,16 +9,19 @@ export function MoviesCards (results){
   const markup = results.map(
     ({ title, id, poster_path, release_date, genre_ids }) => {
       const genreName = renderGenres(genre_ids, allGenres)
+      const year = release_date.slice(0, 4) 
         return `<li class="list card-item">
-            <a href="" class="link"></a>
+            <a href="" class="link">
             <div class="image_thumb">
+              <div class="image_thumb-img">
                 <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${title}" id="${id}" loading="lazy">
-                <div class="content">
+              </div>
+              <div class="content">
                 <h2 class="content_title">${title}</h2>
-                <p class="content_text">${genreName} | ${release_date}</p>
+                <p class="content_text">${genreName} | ${year}</p>
+              </div>
             </div>
-            </div>
-            
+            </a> 
         </li>`}).join(' ');
   refs.cardList.innerHTML = markup;
 };

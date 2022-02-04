eabfef459b989e27b42011027679493959b994f4
Igor Shapoval <azzgard1992@gmail.com>
2022-02-04 18:34:37 +0200

    header/fixed

--- src/JS/header/header-search.js
+++ src/JS/header/header-search.js
@@ -1,5 +1,4 @@
 import ApiServiceFetchMovies from './ApiServiceFetchMovies';
-import { MoviesCards } from '../gallery/gallery-tpl';
 
 const debounce = require('lodash.debounce');
 const apiServiceFetchMovies = new ApiServiceFetchMovies();
@@ -32,4 +31,23 @@ function onSearch(e) {
     refs.error.textContent = '';
     MoviesCards(results);
   });
+}
+
+function MoviesCards(results) {
+  const markup = results
+    .map(({ title, id, poster_path, release_date, genre_ids }) => {
+      return `<li class="list card-item">
+            <a href="" class="link"></a>
+            <div class="image_thumb">
+                <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${title}" id="${id}" loading="lazy">
+                <div class="content">
+                <h2 class="content_title">${title}</h2>
+                <p class="content_text">${genre_ids} | ${release_date}</p>
+            </div>
+            </div>
+            
+        </li>`;
+    })
+    .join(' ');
+  refs.cardList.innerHTML = markup;
 }

f9c16fb7fc44d5821d3269d3b6ec2f02a1d4827b
Denis Fedash <0501054104d@gmail.com>
2022-02-07 14:12:18 +0200

    new gallery features

--- src/js/gallery/fetch-gallery.js
+++ src/js/gallery/fetch-gallery.js
@@ -1,14 +1,7 @@
 const keyAPI = '89b7d98b05de36091af2815feb76deb7'
 
 
-
 export function fetchMovies(page) {
     return fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${keyAPI}&page=${page}`)
         .then(resp => resp.json())        
 }
-
- 
-
-
-
-
--- src/js/gallery/spinner.js
+++
@@ -1,8 +0,0 @@
-const loader = document.querySelector('.loader');
-const cardList = document.querySelector('.card-list')
-
-export const spinner = function () {
-  loader.classList.add('is-hidden');
-  loader.classList.remove('is-hidden');
-};
-

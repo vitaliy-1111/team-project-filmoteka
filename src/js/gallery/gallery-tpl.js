3efdf60d894b0d0cbe5fc24954a5e9c012b5cf0d
Denis Fedash <0501054104d@gmail.com>
2022-02-08 21:35:25 +0200

    add changes

--- src/js/gallery/gallery-tpl.js
+++ src/js/gallery/gallery-tpl.js
@@ -15,15 +15,15 @@ export function MoviesCards (results){
       if (poster_path === null) {
         URL = 'https://ik.imagekit.io/tc8jxffbcvf/default-movie-portrait_EmJUj9Tda5wa.jpg?tr=fo-auto,di-%27;';
       }
-        return `<li class="list card-item">
+      return `<li class="list card-item">
             <div class="image_thumb">
               <img class="image_thumb-img" src="${URL}${poster_path || ''}" alt="${title}" id="${id}" loading="lazy">
-              <span class="content_average">${vote_average}</span>
+              <span class="content_average ${vote_average === 0 ? vote_average='visually-hidden' : vote_average}">${vote_average}</span>
             </div>              
             <div class="gallery-buttons">
-              <button class="image-btn" data-type="watched">Watched</button>
-              <button class="image-btn" data-type="queue">Queue</button>
-              <button class="image-btn see-more" data-type="see more">See more...</button>
+              <button class="image-btn" id="${id}" data-type="watched">Watched</button>
+              <button class="image-btn" id="${id}" data-type="queue">Queue</button>
+              <button class="image-btn see-more" id="${id}" data-type="see more">See more...</button>
             </div>
             <div class="content">
               <h2 class="content_title">${title}</h2>
